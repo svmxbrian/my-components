15801b3c6287d52d77bcfadf76e7cc68ec6a92e8
Brian Lee <brian.lee@servicemax.com>
2017-10-02 10:27:56 -0700

    Fixed Layout tests

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,46 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Layout.scss'
 
-const wrongChildren = `Layout should only contain (1) RegionSidebar and (1) RegionContent components`
-const sidebarNotFirstError = `RegionSidebar must be first child in Layout`
-
-const checkChildren = children => {
-  //Check that children is array (means more than 1 child)
-  if (!children || !Array.isArray(children)) {
-    throw new Error(wrongChildren)
-  }
-
-  //Check if not have 2 children
-  if (children.length !== 2) {
-    throw new Error(wrongChildren)
-  }
-
-  let firstChild = children[0].type.name
-  let secondChild = children[1].type.name
-
-  //Check children order
-  if (firstChild === 'RegionContent' && secondChild === 'RegionSidebar') {
-    throw new Error(sidebarNotFirstError)
-  }
-
-  //Check children types
-  if (firstChild !== 'RegionSidebar' || secondChild !== 'RegionContent') {
-    throw new Error(wrongChildren)
-  }
-}
-
-const Layout = props => {
-  //Check children passed into component
-  if (props) {
-    checkChildren(props.children)
-  }
-
-  return <div className="layout">{props.children}</div>
-}
+const Layout = props => <div className="layout">{props.children}</div>
 
 Layout.propTypes = {
   children: PropTypes.node
 }
 
 export default Layout
-export { sidebarNotFirstError, wrongChildren }
--- src/components/Layout/Layout.test.js
+++ src/components/Layout/Layout.test.js
@@ -1,131 +1,23 @@
 import React from 'react'
-import Layout, { sidebarNotFirstError, wrongChildren } from './Layout'
+import Layout from './Layout'
 import { shallow } from 'enzyme'
-
-import RegionSidebar from 'components/RegionSidebar'
-import RegionContent from 'components/RegionContent'
 
 describe('Layout', () => {
   let layout
   it('renders without crashing', () => {
-    const wrapper = shallow(
-      <Layout>
-        <RegionSidebar />
-        <RegionContent />
-      </Layout>
-    )
+    const wrapper = shallow(<Layout />)
     expect(wrapper).toBeDefined()
   })
 
-  it('should throw error if more than 1 RegionSidebar child', () => {
-    try {
-      layout = shallow(
-        <Layout>
-          <RegionSidebar />
-          <RegionSidebar />
-        </Layout>
-      )
-    } catch (e) {
-      var error = e
-    }
-    expect(error.message).toEqual(wrongChildren)
-
-    try {
-      layout = shallow(
-        <Layout>
-          <RegionSidebar />
-          <RegionContent />
-          <RegionSidebar />
-        </Layout>
-      )
-    } catch (e) {
-      error = e
-    }
-    expect(error.message).toEqual(wrongChildren)
-  })
-
-  it('should throw error if more than 1 RegionContent child', () => {
-    try {
-      layout = shallow(
-        <Layout>
-          <RegionSidebar />
-          <RegionContent />
-          <RegionContent />
-        </Layout>
-      )
-    } catch (e) {
-      var error = e
-    }
-    expect(error.message).toEqual(wrongChildren)
-
-    try {
-      layout = shallow(
-        <Layout>
-          <RegionContent />
-          <RegionContent />
-        </Layout>
-      )
-    } catch (e) {
-      error = e
-    }
-    expect(error.message).toEqual(wrongChildren)
-  })
-
-  it(`should throw error if RegionSidebar not first child`, () => {
-    try {
-      layout = shallow(
-        <Layout>
-          <RegionContent />
-          <RegionSidebar />
-        </Layout>
-      )
-    } catch (e) {
-      var error = e
-    }
-    expect(error.message).toEqual(sidebarNotFirstError)
-
-    try {
-      layout = shallow(
-        <Layout>
-          <div />
-          <RegionSidebar />
-        </Layout>
-      )
-    } catch (e) {
-      error = e
-    }
-    expect(error.message).toEqual(wrongChildren)
-  })
-
-  it(`should throw error if layout does not have 2 children`, () => {
-    try {
-      layout = shallow(<Layout />)
-    } catch (e) {
-      var error = e
-    }
-    expect(error.message).toEqual(wrongChildren)
-
-    try {
-      layout = shallow(
-        <Layout>
-          <div />
-        </Layout>
-      )
-    } catch (e) {
-      error = e
-    }
-    expect(error.message).toEqual(wrongChildren)
-  })
-
   it(`should render its children inside itself`, () => {
-    let sidebar = shallow(<RegionSidebar />)
-    let content = shallow(<RegionContent />)
+    let html = '<div><div>This is a test</div></div>'
     layout = shallow(
       <Layout>
-        <RegionSidebar />
-        <RegionContent />
+        <div>
+          <div>This is a test</div>
+        </div>
       </Layout>
     )
-    expect(layout.html()).toContain(sidebar.html() + content.html())
+    expect(layout.html()).toContain(html)
   })
 })
